5f82979c08a48f2cf1966446a3fa2c3a4f1cebc7
Your Name <you@example.com>
2025-08-17 20:36:21 -0600

    Header Nav Bar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -11,7 +11,7 @@ const Header = () => {
       duration-300"
     >
       <div
-        className="container mx-auto px-4 sm:px-6 lg:px-8 flex items-center justify-between h-16
+        className="container mx-auto px-4 sm:px-6 lg:px-9 flex items-center justify-between h-16
       md:h-20"
       >
         {/* logo/Namem */}
@@ -28,16 +28,15 @@ const Header = () => {
           className="flex items-center"
         >
           <div
-            className="h-10 w-10 rounded-xl bg-gradient-to-r from-gray-500 to-gray 
-            flex items-center justify-center text-red-100 fon-bold text-xl mr-3"
+            className="h-10 w-10 rounded-xl border
+            flex items-center justify-center text-white fon-bold text-xl mr-3"
           >
-            C
+            Rh
           </div>
           <span
-            className="text-xl font-bold bg-gradient-to-r from-gray-500
-           to-gray-700 bg-clip-text text-transparent "
+            className="text-l text-white"
           >
-            Cheyene
+            Rhodie
           </span>
         </motion.div>
 
@@ -56,14 +55,14 @@ const Header = () => {
                   delay: 0.7 + index * 0.2,
                 }}
                 className="relative text-gray-900 dark:text-gray-200 
-                          hover:white-600 dark:hover:text-teal-200 font-medium
+                          hover:white-600 dark:hover:text-white font-medium
                           transition-colors duration-300 group "
                 href="#"
               >
                 {item}
                 <span
                   className="absolute bottom-0 left-0 w-0 h-0.5
-                          bg-teal-200 group-hover:w-full transition-all duration-300"
+                          bg-white group-hover:w-full transition-all duration-300"
                 ></span>
               </motion.a>
             )
@@ -71,12 +70,13 @@ const Header = () => {
         </nav>
 
         {/* social icons */}
+
         <div className="md:flex hidden items-center space-x-9">
           <motion.a
             initial={{ opacity: 0, scale: 0.5 }}
             animate={{ opacity: 1, scale: 1 }}
             transition={{ delay: 1.3, duration: 0.8 }}
-            className="tet-gray-700 dark:text-gray-300 hover:text-teal-600 dark:hover:text-teal-400 transition-colors duration-300"
+            className="tet-gray-700 dark:text-gray-300 hover:text-cyan-600 dark:hover:text-cyan-500 transition-colors duration-300"
             href="#"
           >
             <FiGithub className="w-5 h-5" />
@@ -86,7 +86,7 @@ const Header = () => {
             initial={{ opacity: 0, scale: 0.5 }}
             animate={{ opacity: 1, scale: 1 }}
             transition={{ delay: 1.3, duration: 0.8 }}
-            className="tet-gray-700 dark:text-gray-300 hover:text-teal-600 dark:hover:text-teal-400 transition-colors duration-300"
+            className="tet-gray-700 dark:text-gray-300 hover:text-cyan-600 dark:hover:text-cyan-500 transition-colors duration-300"
             href="#"
           >
             <FiTwitter className="w-5 h-5" />
@@ -96,32 +96,29 @@ const Header = () => {
             initial={{ opacity: 0, scale: 0.5 }}
             animate={{ opacity: 1, scale: 1 }}
             transition={{ delay: 1.3, duration: 0.8 }}
-            className="tet-gray-700 dark:text-gray-300 hover:text-teal-600 dark:hover:text-teal-400 transition-colors duration-300"
+            className="tet-gray-700 dark:text-gray-300 hover:text-cyan-600 dark:hover:text-cyan-500 transition-colors duration-300"
             href="#"
           >
             <FiLinkedin className="w-5 h-5" />
           </motion.a>
+
+          {/* Hire Me Button */}
+
+          <motion.button
+            initial={{ opacity: 0, scale: 0.8 }}
+            animate={{ opacity: 1, scale: 1 }}
+            transition={{
+              delay: 1.6,
+              duration: 0.8,
+              type: "spring",
+              stiffness: 100,
+              damping: 3,
+            }}
+            className="px-4 py-2 rounded-lg bg-black text-white bg-transparent border border-white hover:border-cyan-500 transition-all duration-300"
+          >
+            Let's Build
+          </motion.button>
         </div>
-
-        {/* Hire Me Button */}
-
-        <motion.button
-          initial={{ opacity: 0, scale: 0.8 }}
-          animate={{ opacity: 1, scale: 1 }}
-          transition={{
-            delay: 1.6,
-            duration: 0.8,
-            type: "spring",
-            stiffness: 100,
-            damping: 3,
-          }}
-          className="ml-4 px-4 py-2 rounded-xl bg-gradient-to-r
-          from-teal-500 to-gray-900 text-gray-300 font-bold
-          hover:form-violet-700 hover:to-teal-600 hover:text-white
-          transition-all duration 500 "
-        >
-          Book Me
-        </motion.button>
       </div>
     </header>
   );
